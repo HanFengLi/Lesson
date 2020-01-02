e5fe915bf025a320c8506c58c47932d1436c7f61
lihanfeng <lhf379864428@gmail.com>
2020-01-02 16:32:05 +0900

    update

---
+++ LessonCode/LessonCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LessonCode
+//
+//  Created by 李晗峰 on 2020/1/2.
+//  Copyright © 2020 lihanfeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
